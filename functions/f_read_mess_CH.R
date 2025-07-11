306688f4accddc1bae06040514d8025d804eb2f5
m-kehl <rudolfsinger36@gmail.com>
2025-07-11 09:10:34 +0000

    read in measurement data of swiss surface measurement stations for current day

--- functions/f_read_mess_CH.R
+++ functions/f_read_mess_CH.R
@@ -1,12 +1,13 @@
 f_read_mess_CH <- function(station_name,mess_meta_CH){
 
 
-  station_abbr <- mess_meta_CH$station_abbr[mess_meta_CH$station_name==station_name]
+  station_abbr <- mess_meta_CH$station_abbr[mess_meta_CH$Stationsname==station_name]
   station_abbr <- tolower(station_abbr)
   URL <- paste0(
     "https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn/",station_abbr,
     "/ogd-smn_",station_abbr,"_t_now.csv"
   )
   mess_data_CH <- read.csv(URL, sep=";")
+  colnames(mess_data_CH)[2] <- "MESS_DATUM"
 
 }
