12e5caac9871cd1f130a7c337b6cd406be5f913e
m-kehl <rudolfsinger36@gmail.com>
2025-07-11 09:11:03 +0000

    adapt read in function so they can cope with Switzerland option

--- functions/f_read_mess_meta.R
+++ functions/f_read_mess_meta.R
@@ -1,13 +1,18 @@
-f_read_mess_meta <- function(){
-  ## function to read measurement meta data
-  #source
-  mess_base <- "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/air_temperature/now/"
-  mess_meta <- f_readDWD.meta(paste0(mess_base,"zehn_now_tu_Beschreibung_Stationen.txt"))
+f_read_mess_meta <- function(country){
+  if (country == "Schweiz"){
+    mess_meta <- f_read_mess_meta_CH()
+  } else{
+    ## function to read measurement meta data
+    #source
+    mess_base <- "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/air_temperature/now/"
+    mess_meta <- f_readDWD.meta(paste0(mess_base,"zehn_now_tu_Beschreibung_Stationen.txt"))
 
-  #sort
-  mess_meta <- arrange(mess_meta,Stationsname, .locale = "de")
-  #format stations id
-  mess_meta$Stations_id <- sprintf("%0*d", 5, mess_meta$Stations_id)
+    #sort
+    mess_meta <- arrange(mess_meta,Stationsname, .locale = "de")
+    #format stations id
+    mess_meta$Stations_id <- sprintf("%0*d", 5, mess_meta$Stations_id)
+  }
+
   return(mess_meta)
 }
 
