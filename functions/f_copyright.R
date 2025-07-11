ec117e9753fce6192df7447e968030de644e9f2e
m-kehl <rudolfsinger36@gmail.com>
2025-07-11 20:00:46 +0000

    copyright MeteoSwiss and DWD

--- functions/f_copyright.R
+++ functions/f_copyright.R
@@ -1,4 +1,4 @@
-f_copyright_DWD <- function(){
+f_copyright <- function(){
   #' copywrite message
   #'
   #' write DWD copywrite message using Unicode character for copyright sign
@@ -8,5 +8,5 @@ f_copyright_DWD <- function(){
   #' copywrite <- f_copyright_DWD();
   #'
   #'
-  p("Datenbasis: \u00A9 Deutscher Wetterdienst (opendata.dwd.de)")
+  p("Datenbasis: \u00A9 MeteoSwiss (data.geo.admin.ch), \u00A9 Deutscher Wetterdienst (opendata.dwd.de)")
 }
