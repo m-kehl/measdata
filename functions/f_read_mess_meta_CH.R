be7636bbfc3669e15263ec1b869e0ad1785717bd
m-kehl <rudolfsinger36@gmail.com>
2025-07-11 09:10:12 +0000

    function to read in meta data of swiss surface measurement stations

--- functions/f_read_mess_meta_CH.R
+++ functions/f_read_mess_meta_CH.R
@@ -3,6 +3,7 @@ f_read_mess_meta_CH <- function(){
   #source
   mess_meta_CH <- read.csv("https://data.geo.admin.ch/ch.meteoschweiz.ogd-smn/ogd-smn_meta_stations.csv",
                            sep = ";",encoding = "latin1")
+  colnames(mess_meta_CH)[2] <- "Stationsname"
 
   return(mess_meta_CH)
 }
